b6bd4d2a70c021f4664531fca5dc0cbb07620c31
unknown <RockstarJay>
2021-06-04 16:47:20 +0530

    Results of Heads and Tails as Coin flipped Multiple Times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 echo "Flip Coin Simulation Problem"
 
+read -p "How Many Time you Want To Flip The Coin : " n
 isHead=1
-flipCoin=$((RANDOM%2))
-if [ $flipCoin -eq $isHead ]
-then
-	echo "Head Wins"
-else
-	echo "Tail Wins"
-fi
+Heads=0
+Tails=0
+for (( i=0; i<n; i++ ))
+do
+	flipCoin=$((RANDOM%2))
+	if [ $flipCoin -eq $isHead ]
+	then
+		((Heads++))
+	else
+		((Tails++))
+	fi
+done
+echo "Head Wins $Heads Times"
+echo "Tail Wins $Tails Times"
